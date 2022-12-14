1f117a427da6541d84d2a7a28158b2054b99fd28
AlexIstra1989 <alex888fcsm@gmail.com>
2022-12-14 23:57:09 +0300

    added wholesale-page

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -180,6 +180,85 @@ function initSliders() {
 			}
 		});
 	}
+ if (document.querySelector('.projects__slider')) { // Указываем скласс нужного слайдера
+		// Создаем слайдер
+		new Swiper('.projects__slider', { // Указываем скласс нужного слайдера
+			// Подключаем модули слайдера
+			// для конкретного случая
+			modules: [Navigation, Pagination],
+			observer: true,
+			observeParents: true,
+			slidesPerView: 1,
+			spaceBetween: 0,
+			autoHeight: true,
+			speed: 800,
+
+			//touchRatio: 0,
+			//simulateTouch: false,
+			//loop: true,
+			//preloadImages: false,
+			//lazy: true,
+
+			// Эффекты
+			// effect: 'fade',
+			// autoplay: {
+			// 	delay: 3000,
+			// 	disableOnInteraction: false,
+			// },
+
+			// Пагинация
+			pagination: {
+				el: '.swiper-pagination',
+				clickable: true,
+			},
+
+			// Скроллбар
+			/*
+			scrollbar: {
+				el: '.swiper-scrollbar',
+				draggable: true,
+			},
+			*/
+
+			// Кнопки "влево/вправо"
+			navigation: {
+				prevEl: '.swiper-button-prev',
+				nextEl: '.swiper-button-next',
+			},
+
+			// Брейкпоинты
+
+			breakpoints: {
+				// 320: {
+				// 	slidesPerView: 1,
+				// 	spaceBetween: 0,
+				// 	autoHeight: true,
+				// },
+    // 500: {
+				// 	slidesPerView: 2,
+				// 	spaceBetween: 20,
+				// 	autoHeight: true,
+				// },
+				// 700: {
+				// 	slidesPerView: 3,
+				// 	spaceBetween: 30,
+				// },
+				// 900: {
+				// 	slidesPerView: 4,
+				// 	spaceBetween: 30,
+				// },
+				// 1268: {
+				// 	slidesPerView: 4,
+				// 	spaceBetween: 30,
+				// },
+			},
+   
+			// События
+			on: {
+
+			}
+		});
+	}
 }
 // Скролл на базе слайдера (по классу swiper_scroll для оболочки слайдера)
 function initSlidersScroll() {
