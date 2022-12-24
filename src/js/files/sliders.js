27492b53e9bd91df74964bec1a9c91207424f743
AlexIstra1989 <alex888fcsm@gmail.com>
2022-12-24 22:57:21 +0300

    added catalog-page css and adaptive

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -168,10 +168,6 @@ function initSliders() {
 					slidesPerView: 4,
 					spaceBetween: 30,
 				},
-				// 1268: {
-				// 	slidesPerView: 4,
-				// 	spaceBetween: 30,
-				// },
 			},
    
 			// События
@@ -251,6 +247,81 @@ function initSliders() {
 				// 	slidesPerView: 4,
 				// 	spaceBetween: 30,
 				// },
+			},
+   
+			// События
+			on: {
+
+			}
+		});
+	}
+ if (document.querySelector('.viewed__slider')) { // Указываем скласс нужного слайдера
+		// Создаем слайдер
+		new Swiper('.viewed__slider', { // Указываем скласс нужного слайдера
+			// Подключаем модули слайдера
+			// для конкретного случая
+			modules: [Navigation, Pagination],
+			observer: true,
+			observeParents: true,
+			slidesPerView: 4,
+			spaceBetween: 30,
+			autoHeight: true,
+			speed: 800,
+
+			//touchRatio: 0,
+			//simulateTouch: false,
+			//loop: true,
+			//preloadImages: false,
+			//lazy: true,
+
+			// Эффекты
+			// effect: 'fade',
+			// autoplay: {
+			// 	delay: 3000,
+			// 	disableOnInteraction: false,
+			// },
+
+			// Пагинация
+			pagination: {
+				el: '.swiper-pagination',
+				clickable: true,
+			},
+
+			// Скроллбар
+			/*
+			scrollbar: {
+				el: '.swiper-scrollbar',
+				draggable: true,
+			},
+			*/
+
+			// Кнопки "влево/вправо"
+			navigation: {
+				prevEl: '.swiper-button-prev',
+				nextEl: '.swiper-button-next',
+			},
+
+			// Брейкпоинты
+
+   breakpoints: {
+				320: {
+					slidesPerView: 1,
+					spaceBetween: 0,
+					autoHeight: true,
+				},
+    500: {
+					slidesPerView: 2,
+					spaceBetween: 20,
+					autoHeight: true,
+				},
+				700: {
+					slidesPerView: 3,
+					spaceBetween: 30,
+				},
+				900: {
+					slidesPerView: 4,
+					spaceBetween: 30,
+				},
 			},
    
 			// События
